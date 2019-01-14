ee5d236a78ba2ba6215f83d62ed90e7060bd3e50
shuunz <shuunz120@gmail.com>
2019-01-14 10:51:20 +0300

    controller

---
+++ controllers/itemController.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const items = require('../models/items');
+
+const sendItems = (req,res) => res.json(res.locals.allItem);
+const sendItem = (req,res) => res.json(res.locals.item);
+const sendSuccess = (req,res) => res.json({message :'success'});
+
+
+
+router.get('/',items.getAll,sendItems);
+router.get('/:id',item.find,sendItem);
+router.post('/',item.create,sendItem);
+router.put('/:id',item.find,sendItem);
+router.delete('/:id',item.delete,sendSuccess);
+module.exports = router;
