776a5b5194ae7e15e4bb272a10cbc9f0ca499437
arfajraa <arfajraa@gmail.com>
2019-01-14 10:34:16 +0300

    made some modifications to config and fixed seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,27 +1,8 @@
-DROP DATABASE IF EXISTS lost_and_found
+DROP DATABASE IF EXISTS lost_and_found;
 
-CREATE DATABASE lost_and_found
+CREATE DATABASE lost_and_found;
 
-\c
-
-CREATE TABLE items(
-
-id SERIAL PRIMARY KEY,
-name VARCHAR,
-type VARCHAR,
-imageUrl VARCHAR,
-lat DOUBLE PRECISION,
-lon DOUBLE PRECISION,
-addedBy VARCHAR,
-receivedBy VARCHAR,
-FOREIGN KEY(addedBy) REFERENCES contact,
-FOREIGN KEY(receivedBy) REFERENCES contact,
-addedDate DATE,
-
-);
-
-INSERT INTO items(name, type, imageUrl, lat, lon, addedBy, receivedBy, addedDate)
-VALUES('mobile','found','https://www.pexels.com/photo/nature-red-love-romantic-67636/',166.57 , 156.74 ,'Ahmad','Mohammad','12/01/2019');
+\c lost_and_found
 
 
 CREATE TABLE contact(
@@ -31,5 +12,28 @@ name VARCHAR,
 phone VARCHAR,
 email VARCHAR
 );
-INSERT INTO contact(name, phone, email)VALUES('Ahmad','05467990','abcd@hotmail.com');
+
+CREATE TABLE items(
+
+id SERIAL PRIMARY KEY,
+name VARCHAR,
+type VARCHAR,
+imageUrl VARCHAR,
+lat DOUBLE PRECISION,
+lon DOUBLE PRECISION,
+addedBy INTEGER,
+receivedBy INTEGER,
+addedDate DATE,
+FOREIGN KEY(addedBy) REFERENCES contact,
+FOREIGN KEY(receivedBy) REFERENCES contact
+);
+
+
+INSERT INTO contact(name, phone, email)VALUES('Ahmad','05467990','abcd@hotmail.com'),
+('Mohammed', '0554279362', 'mohammed@gmail.com');
+
+
+INSERT INTO items(name, type, imageUrl, lat, lon, addedBy, receivedBy, addedDate)
+VALUES('mobile','found','https://www.pexels.com/photo/nature-red-love-romantic-67636/',166.57 , 156.74 , 1, 2,'12/01/2019'),
+('laptop', 'lost', 'https://target.scene7.com/is/image/Target/GUEST_064d9050-5243-4fc9-b689-9cb0b68d15bf?wid=488&hei=488&fmt=pjpeg', 180.59, 160.30, 2, 1, '12/08/2019');
 
