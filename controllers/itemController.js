bf0517b1f3a163e8992ae9315293c0ebaf8d1963
AlanoudBinSalamah <alanoud.binsalamah@gmail.com>
2019-01-14 10:57:15 +0300

    controller

---
+++ controllers/itemController.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const item =require('../models/item');
+
+const sendItems = (req,res)=>res.json(res.locals.items);
+const sendItem = (req,res)=> res.json(res.locals.item);
+// const sendSuccess=(req,res)=> res.json({message:'success'});
+
+router.get('/',item.getAll,sendItems);
+router.post('/', item.create,sendItem);
+router.put('/:id', item.update,sendItem );
+// router.delete('/:id',sendSucces);
+
+module.exports=router;
